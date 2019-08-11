e62e103e79c19bbddc3a70c30bd90abf96e52843
Mikolaj Miller <mikolaj.miller@kcl.ac.uk>
2019-08-11 20:36:43 +0100

    Implement navbar using Bootstrap

--- src/main/java/com/medical/controllers/HomeController.java
+++ src/main/java/com/medical/controllers/HomeController.java
@@ -3,20 +3,23 @@ package com.medical.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medical.models.User;
 import com.medical.repos.UserRepo;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@Autowired
 	private UserRepo userRepo;
 	
 	@GetMapping("/")
-	public List<User> home(){
-		return userRepo.findAll();
+	public String home(Model m){
+		m.addAttribute("result",userRepo.findAll());
+		return "home";
 	}
 }
